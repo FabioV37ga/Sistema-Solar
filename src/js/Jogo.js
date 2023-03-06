04a5f341788bb9a884158d3537a2fcce33fa6457
Fábio Veiga <veigabfabio@hotmail.com>
2023-03-06 13:59:30 -0200

    Documentação

--- src/js/Jogo.js
+++ src/js/Jogo.js
@@ -1,3 +1,31 @@
+/*
+Classe Jogo
+    - Essa classe é responsável por criar o elemento que comporta o jogo.
+    - Também é responsável por receber os inputs do usuário [↑w][↓s][←a][→d][space]
+Índice
+    1 Atributos
+        1.1 elemento_jquery_jogo → string elemento 'sect.jogo>div.playArea'
+        1.2 elemento_html_jogo → elemento html .jogo
+        1.3 elemento_html_janela → elemento html .playArea
+        1.4 faseAtual → Armazena fase atual.
+        1.5 enableMove → Alterna entre movimento da dave ativado e desativado. (0 , 1)
+        1.6/7 moveState_x/y → Armazena informação do estado do movimento da nave de x e y.
+    2 Métodos
+        2.1 criar(tipo) → Cria elementos 'elementos_jquery_jogo', chama 'selecionar()' e chama 'nave.criar()'
+        2.2 selecionar() → Atribui os elementos html aos atributos do tipo '_html'
+        2.3 iniciar() → Inicia jogo. chama 'tutorial(1)'.
+        2.4 tutorial() → Mostra os tutoriais ao longo do jogo. 
+            [1]: Tutorial de movimentação. Aguarda input de [↑w][↓s][←a][→d] para chamar 'ativarMovimentacao()'
+                 e também 'avancarFase()'.
+            [2]: Tutorial de combate. Trava movimentação e aguarda input de [Space] para liberar movimentação.
+        2.5 ativarMovimentacao() → Adiciona função as teclas [↑w][↓s][←a][→d], em resumo, existe uma função que
+                                   é adicionada ao 'keydown' das teclas que ativa a movimentação no eixo respectivo 
+                                   e outra que é adicionada ao 'keyup' das teclas que desativa a movimentação no 
+                                   eixo respectivo. Existem também outras regras p/ fazer  um "overlapse" da direção
+                                   baseado nas teclas pressionadas e soltas;
+        2.6 avancarFase() → Adiciona +1 na fase atual (inicializada como 0). Também cria uma instância da 
+                          classe Fase com informações da fase atual.
+*/
 class Jogo {
     static elemento_jquery_jogo = `
     <section class="jogo">
