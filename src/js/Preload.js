b3aeb3b54d71eff9245ee5e7c0d4059b3c877abe
Fábio Veiga <veigabfabio@hotmail.com>
2023-03-07 15:35:11 -0200

    Ajustes no preload

--- src/js/Preload.js
+++ src/js/Preload.js
@@ -1,14 +1,19 @@
 // Esse arquivo é responsável por carregar recursos do jogo anteriormente para evitar instabilidade
 $("main").append($("<div>", { class: 'preload'}))
-$(".preload").append($("<img>", { src: 'src/img/boom_1.png', style: 'opacity: 0' }))
-$(".preload").append($("<img>", { src: 'src/img/boom_2.png', style: 'opacity: 0' }))
-$(".preload").append($("<img>", { src: 'src/img/boom_3.png', style: 'opacity: 0' }))
-$(".preload").append($("<img>", { src: 'src/img/boom_4.png', style: 'opacity: 0' }))
-$(".preload").append($("<img>", { src: 'src/img/boom_5.png', style: 'opacity: 0' }))
-$(".preload").append($("<img>", { src: 'src/img/boom_6.png', style: 'opacity: 0' }))
-$(".preload").append($("<img>", { src: 'src/img/boom_7.png', style: 'opacity: 0' }))
-$(".preload").append($("<img>", { src: 'src/img/boom_8.png', style: 'opacity: 0' }))
+console.log(`[#PRELOAD] iniciado`)   
 
-setTimeout(() => {
-    $(".preload")[0].remove()
-}, 100);
+for(let i = 1; i <= 8; i++){
+    // imagens de explosão
+    $(".preload").append($("<img>", { src: `src/img/boom_${i}.png`, style: 'opacity: 0', class: "boom"})) 
+    console.log(`[#PRELOAD] ${i} ✅`)
+}
+
+if($(".boom").length == 8){
+    setTimeout(() => {
+        console.clear()
+        console.log(`[#PRELOAD] Concluído ✅`)
+    }, 2000);
+    setTimeout(() => {
+        $(".preload")[0].remove()
+    }, 100);
+}
