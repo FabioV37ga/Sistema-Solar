61e483ed987da21ad935a4f6b0e0e5794f507acd
Fábio Veiga <veigabfabio@hotmail.com>
2023-03-07 19:36:52 -0200

    Animação do background (Lógica de progresso)

--- src/js/Fase.js
+++ src/js/Fase.js
@@ -1,4 +1,9 @@
 class Fase {
+    static progressao = 0;
+    numero;
+    inimigos;
+    duracao;
+    planeta;
     constructor(numero, inimigos, duracao) {
         this.numero = numero;
         this.inimigos = inimigos.split('').length > 1 ? inimigos.split(",") : inimigos
@@ -10,17 +15,24 @@ class Fase {
     iniciar() {
         console.log(`[#${this.numero}] Fase iniciada. \n`)
         console.log(`[#${this.numero}] Viagem inciada: Planeta anterior → Confronto atual \n`)
+        Fase.progressao = 1
         this.viajar(1)
     }
 
     viajar(arg) {
-        // Fase.janela = document.querySelector(".janela");
-        // var posicaoAtual = parseInt(document.querySelector(".janela").style.backgroundPositionX.toString().replace("px",""))
-        // var intervalo = setInterval(() => {
-        //     posicaoAtual -= this.velocidade;
-        //     this.janela.style = `background-position-x: ${posicaoAtual}px;`
-        //     console.log("Avanço atual: "+this.avanco)
-        // }, 1);
+
+        Fase.janela = document.querySelector(".janela");
+        var posicaoAtual = parseInt(Fase.janela.style.backgroundPositionX.toString().replace("px", ""))
+        var intervalo = setInterval(() => {
+            if (Fase.progressao == 1){
+                posicaoAtual--
+                Fase.janela.style = `background-position-x: ${posicaoAtual}px;`
+            }else{
+                clearInterval(intervalo)
+            }
+            // console.log("Avanço atual: " + posicaoAtual)
+        }, 1);
+
         if (arg == 1) {
             setTimeout(() => {
                 console.log(`[#${this.numero}] Viagem Finalizada: Planeta anterior → Confronto atual \n`)
