5cc0b42284173d637e80ae4871d7c83f7ac13d88
Fábio Veiga <veigabfabio@hotmail.com>
2023-03-13 13:09:50 -0200

    Informações de todos os planetas

--- src/js/Planeta.js
+++ src/js/Planeta.js
@@ -22,51 +22,51 @@ class Planeta {
     [
         {
             "id": 1,
-            "nome": "mercurio",
-            "tamanho": "null km",
-            "texto": "Baita planeta. nota 6"
+            "nome": "Mercúrio",
+            "tamanho": "4.879,4 km",
+            "texto": "É o menor planeta do Sistema Solar. Apresenta corpo rochoso. Um dia em Mercúrio dura 59 dias na Terra, um ano dura 88 dias na Terra. Não possui satélites naturais. Núcleo formado predominantemente de ferro. Mesmo tão próximo do Sol, sondas encontraram gelo em Mercúrio."
         },
         {
             "id": 2,
-            "nome": "venus",
-            "tamanho": "null km",
-            "texto": "Baita planeta. nota 6"
+            "nome": "Vênus",
+            "tamanho": "12.104 km",
+            "texto": "Sua atmosfera é composta de dióxido de carbono. Não há placas tectônicas ativas em Vênus, e seu dia dura cerca de 243 dias na terra. Vênus é o planeta mais quente do Sistema Solar, com uma temperatura de superfície que pode chegar a 462 graus."
         },
         {
             "id": 3,
-            "nome": "terra",
-            "tamanho": "null km",
-            "texto": "Baita planeta. nota 6"
+            "nome": "Terra",
+            "tamanho": "12.742 km",
+            "texto": "Casa. Tudo o que conhecemos como vida está aqui. É o maior dos planetas rochosos do Sistema Solar. É composto principalmente de rochas e minerais, além de água e ar. O núcleo é principalmente de ferro. Sua atmosfera é cerca 78% de nitrogênio e 21% de oxigênio."
         },
         {
             "id": 4,
-            "nome": "marte",
-            "tamanho": "null km",
-            "texto": "Baita planeta. nota 6"
+            "nome": "Marte",
+            "tamanho": "6.779 km",
+            "texto": "Sua superfície é coberta por crateras, montanhas e vales. Composto principalmente de rochas e óxidos metálicos, como ferro e magnésio. Acredita-se que seu núcleo é de níquel e ferro. Além da terra, é o único planeta que apresenta estações climáticas diferentes em um ano. Seu dia dura 39 minutos a mais que a terra."
         },
         {
             "id": 5,
-            "nome": "jupiter",
-            "tamanho": "null km",
-            "texto": "Baita planeta. nota 6"
+            "nome": "Júpiter",
+            "tamanho": "139.820 km",
+            "texto": "Cabem cerca de 1320 planetas Terra em Júpiter. É composto principalmente de hidrogênio e hélio. Acredita-se que seu núcleo seja composto de rocha, gelo e metais pesados. Possui diversas luas e um sistema de anéis. Existe um furacão maior do que a Terra em júpiter que está ativo a mais de 300 anos. Um dia aqui equivale 9h56m na terra."
         },
         {
             "id": 6,
-            "nome": "saturno",
-            "tamanho": "null km",
-            "texto": "Baita planeta. nota 6"
+            "nome": "Saturno",
+            "tamanho": "116.460 km",
+            "texto": "Famoso por seus anéis brilhantes. Composto principalmente de hidrogênio e hélio. Saturno é o planeta menos denso do sistema solar, se houvesse um oceano grande suficiente para colocá-lo, ele flutuaria. Seus anéis possuem gelo e rocha maiores que edifícios. Um dia em saturno equivale 10 horas e 40 minutos na terra."
         },
         {
             "id": 7,
-            "nome": "urano",
-            "tamanho": "null km",
-            "texto": "Baita planeta. nota 6"
+            "nome": "Urano",
+            "tamanho": "50.724 km",
+            "texto": "Gigante de gás e gelo. Composto de hidrogênio e hélio, um pouco de metano e outros gases. Acredita-se que o núcleo seja composto de rochas e envolvido por água, amônia e metano. Possui estações climáticas que duram cerca de 21 anos cada. Um dia em Urano dura cerca de 17 horas e 14 minutos na Terra."
         },
         {
             "id": 8,
-            "nome": "netuno",
-            "tamanho": "null km",
-            "texto": "Baita planeta. nota 6"
+            "nome": "Netuno",
+            "tamanho": "49.244 km",
+            "texto": "Localizado na região externa do sistema solar. Composição parecida com urano. Acredita-se que o núcleo seja composto de rochas, gelo e metais pesados. Ventos em saturno podem chegar em até 2.000km/h. Possui uma lua chamada Tritão, que é coberta de gelo e tem vulcões ativos. Um dia em Netuno dura cerca de 16 horas e 6 minutos na Terra."
         }
     ]`
 
@@ -79,11 +79,14 @@ class Planeta {
         const planeta = JSON.parse(Planeta.planetas)
         var planetaAtual = planeta.find(planeta => planeta.id === faseAtual)
         // Cria area e imagem do planeta
+
+        var nomeFormatado = planetaAtual.nome.toString().toLowerCase().replace("ú", "u").replace("ê", "e")
+
         if (parte == 1) {
             $(".jogo").append(`
                 <div class="planetarea">
                     <div class="planeta">
-                        <img src="src/planetas/outro/${planetaAtual.id}_${planetaAtual.nome}.svg">
+                        <img src="src/planetas/outro/${planetaAtual.id}_${nomeFormatado}.svg">
                     </div>
                 </div>
              `)
@@ -101,7 +104,7 @@ class Planeta {
                             <h1 class="info-title">${planetaAtual.nome}</h1>
                             <div class="info-size">Tamanho: <br> ${planetaAtual.tamanho}</div>
                             <div class="info-text">
-                                <h1>Informacoes Gerais</h1>
+                                <h1>Informações Gerais</h1>
                                 <p>${planetaAtual.texto}
                                 </p>
                             </div>
